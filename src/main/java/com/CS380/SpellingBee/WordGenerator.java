171ebd920d6e2696305c610d8871a2672916f38d
Tobias Ward <tate@tatesworld.com>
2023-03-02 23:17:43 -0600

    Updated WordGenerator.java
    Added a helpful comment detailing how one gets the eclipse project to match what’s in the filesystem and what’s on GitHub.
    
    Signed-off-by: Tobias Ward <tate@tatesworld.com>

--- src/main/java/com/CS380/SpellingBee/WordGenerator.java
+++ src/main/java/com/CS380/SpellingBee/WordGenerator.java
@@ -2,4 +2,12 @@ package com.CS380.SpellingBee;
 
 public class WordGenerator {
 
+	/*
+	 * This is a comment in the new Word Generator!
+	 * 
+	 * 
+	 * After each pull, we need to right click on the project then select -> run as -> maven clean
+	 * then select project clean from the menu at the top
+	 * then the project should be up to date with the files in the filesystem!
+	 */
 }
