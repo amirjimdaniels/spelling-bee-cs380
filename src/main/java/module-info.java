e07e04374281b8c76e4bbc22ac6f7a66e9759ebe
leahykieran <kleahy@austincollege.edu>
2023-03-04 21:08:17 -0600

    fixed module error

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.CS380.SpellingBee {
     requires javafx.controls;
     requires javafx.fxml;
-	requires junit;
+
 
     opens com.CS380.SpellingBee to javafx.fxml;
     exports com.CS380.SpellingBee;
