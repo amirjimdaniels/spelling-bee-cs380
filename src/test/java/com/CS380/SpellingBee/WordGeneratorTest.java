7196628407ea51d2d9772aba80002a91f9d82ae0
Tobias Ward <tate@tatesworld.com>
2023-03-06 13:41:28 -0600

    Create Test Class for Word Generator
    Unimplemented - still need to write tests
    
    Signed-off-by: Tobias Ward <tate@tatesworld.com>

---
+++ src/test/java/com/CS380/SpellingBee/WordGeneratorTest.java
@@ -0,0 +1,54 @@
+package com.CS380.SpellingBee;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WordGeneratorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testWordGenerator() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCreateDailyWords() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSelectMainWord() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetEligibleWords() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetDailyWords() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetAllWords() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSetDailyWords() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSetAllWords() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
