70f0ee1b1c82aae6fbc39878d27ce0f6e8935a5b
Alwin224 <alwinbhogal2@gmail.com>
2023-03-06 11:54:12 -0600

    Made a css file

--- src/main/java/com/CS380/SpellingBee/App.java
+++ src/main/java/com/CS380/SpellingBee/App.java
@@ -18,6 +18,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
+        scene.getStylesheets().add("/spelling-bee-cs380/src/main/resources/com/CS380/SpellingBee/SpellingBee.css");
         stage.setScene(scene);
         stage.show();
     }
